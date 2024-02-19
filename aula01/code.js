a604b2bb8b59c90f8ecc73dcb0fad094a4b5c3ea
Miguel Luiz Sommerfeld <125596745+miguelcfh@users.noreply.github.com>
2024-02-19 16:55:32 -0300

    aula01
    JavaScript e HTML

---
+++ aula01/code.js
@@ -0,0 +1,10 @@
+function cnome() {
+    let n = document.getElementById(nome).value
+    alert("Bem vindo ao JS: " + n)
+}
+
+
+function ola() {
+    console.log("Olá MUndo!")
+    alert("Meu Front-End")
+}
